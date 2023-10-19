7ef6305f6aea3b9511c77b2abe5c7929b083a4ea
이찬솔 <58240527+chansol790@users.noreply.github.com>
2023-10-19 09:26:39 +0900

    add crawling contents

--- job02_crawling_bookcontents.py
+++ job02_crawling_bookcontents.py
@@ -1,13 +1,17 @@
 import glob
 import re
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver import ActionChains
 import pandas as pd
-import time
+from selenium.webdriver.support import expected_conditions as EC
 
 options = ChromeOptions()
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
@@ -16,6 +20,7 @@ options.add_argument("lang=ko_KR")
 
 service = ChromeService(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=options)
+wait = WebDriverWait(driver, 5)
 
 data_path = glob.glob('./crawling_data/*data*.csv')
 link_path = glob.glob('./crawling_data/*link*.csv')
@@ -38,12 +43,11 @@ for link in link_path:
         try:
             actions = driver.find_element(By.CSS_SELECTOR, 'body')
             actions.send_keys(Keys.END)
-            time.sleep(0.5)
+            time.sleep(1)
         except:
             continue
         try:
-            bk_title = driver.find_element('xpath',
-                                           '//*[@id="Ere_prod_allwrap"]/div[3]/div[2]/div[1]/div/ul/li[2]/div/span').text
+            bk_title = driver.find_element('xpath', '//*[@id="Ere_prod_allwrap"]/div[3]/div[2]/div[1]/div/ul/li[2]/div/span').text
             bk_title = re.compile('[^가-힣]').sub(' ', bk_title)
             titles.append(bk_title)
         except:
@@ -51,6 +55,7 @@ for link in link_path:
         for j in range(1,10):
             for k in range(1,10):
                 try:
+                    act = ActionChains(driver)
                     title = driver.find_element('xpath', '//*[@id="Ere_prod_allwrap"]/div[{}]/div[{}]/div[1]'.format(j,k)).text
                     if '책소개' in title:
                         try:
@@ -70,7 +75,9 @@ for link in link_path:
             df_contents = pd.DataFrame(contents, columns=['contents'])
             df_contents['titles'] = titles
             df_contents.to_csv('./crawling_data/crawling_last_{}.csv'.format(count), index=False)
+    df_contents = pd.DataFrame(contents, columns=['contents'])
+    df_contents['titles'] = titles
+    df_contents.to_csv('./crawling_data/crawling_final_{}.csv'.format(link), index = False)
     contents = []
     titles = []
-    df_contents.to_csv('./crawling_data/crawling_final_{}.csv'.format(link), index = False)
     count = 0
